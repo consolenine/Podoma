027334e76bf62ddf353928470ac2286ad2ded54a
Shashank Deep <48642522+consolenine@users.noreply.github.com>
2024-02-20 17:35:58 +0530

    Fixed Responsive Issues

--- js/main.js
+++ js/main.js
@@ -8,7 +8,35 @@ $(document).ready(function() {
         // If we need pagination
         pagination: {
             el: '.swiper-pagination',
-        }
+        },
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 20
+            },
+            // when window width is >= 480px
+            480: {
+                slidesPerView: 2,
+                spaceBetween: 30
+            },
+            // when window width is >= 640px
+            640: {
+                slidesPerView: 3,
+                spaceBetween: 40
+            }
+        },
+        centeredSlides: true
+    });
+
+    const featuredProductSwiper = new Swiper('.featured-product-swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        slidesPerView: 1,
+        // autoplay: {
+        //     delay: 2000
+        // }
     });
     
 })
