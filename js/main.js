5dbe3e992843339dd96f420467bc59eab630473b
Shashank Deep <48642522+consolenine@users.noreply.github.com>
2024-02-28 18:59:15 +0530

    Changes To Layout

--- js/main.js
+++ js/main.js
@@ -1,6 +1,94 @@
 $(document).ready(function() {
-    AOS.init();
-    const swiper = new Swiper('.swiper', {
+    const featuredProductSwiper = new Swiper('.featured-product-swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        slidesPerView: 1,
+        effect: "fade",
+        // autoplay: {
+        //     delay: 2000
+        // },
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 0
+            },
+            // when window width is >= 480px
+            480: {
+                slidesPerView: 1,
+                spaceBetween: 0
+            },
+            // when window width is >= 640px
+            640: {
+                slidesPerView: 1,
+                spaceBetween: 0
+            }
+        },
+    });
+    const categorySwiper = new Swiper('.category-swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        slidesPerView: 3,
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 20
+            },
+            // when window width is >= 480px
+            600: {
+                slidesPerView: 2,
+                spaceBetween: 40
+            },
+            // when window width is >= 640px
+            1200: {
+                slidesPerView: 3,
+                spaceBetween: 60
+            }
+        },
+        centeredSlides: true
+    });
+    const instagramSwiper = new Swiper('.instagram-swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        slidesPerView: 1,
+        // If we need pagination
+        autoplay: {
+            delay: 2000
+        },
+        effect: 'fade',
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 40
+            },
+            // when window width is >= 480px
+            600: {
+                slidesPerView: 1,
+                spaceBetween: 30
+            },
+            // when window width is >= 640px
+            1200: {
+                slidesPerView: 1,
+                spaceBetween: 40
+            }
+        },
+        centeredSlides: true
+    });
+
+    const tipsSwiper = new Swiper('.tips-swiper', {
         // Optional parameters
         direction: 'horizontal',
         loop: true,
@@ -13,30 +101,20 @@ $(document).ready(function() {
             // when window width is >= 320px
             320: {
                 slidesPerView: 1,
-                spaceBetween: 20
+                spaceBetween: 40
             },
             // when window width is >= 480px
-            480: {
+            600: {
                 slidesPerView: 2,
                 spaceBetween: 30
             },
             // when window width is >= 640px
-            640: {
+            1200: {
                 slidesPerView: 3,
                 spaceBetween: 40
             }
         },
         centeredSlides: true
-    });
-
-    const featuredProductSwiper = new Swiper('.featured-product-swiper', {
-        // Optional parameters
-        direction: 'horizontal',
-        loop: true,
-        slidesPerView: 1,
-        // autoplay: {
-        //     delay: 2000
-        // }
     });
     
 })
